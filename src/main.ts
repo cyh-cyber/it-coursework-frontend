e9ce500ef3a40c72a4998c55e0d1cbbb07a591ef
ddd DDD <3107816X@student.gla.ac.uk>
2026-03-09 19:51:56 +0000

    fixed text error

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 const app = createApp(App)
 
